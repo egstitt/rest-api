aba8e586c0c02f5a32f3e7555eb592a96d6e039f
egstitt <evangstitt@gmail.com>
2016-10-28 15:12:43 -0700

    Wired up to RDS. Added very basic basic auth.

--- src/main/java/com/specialized/SpringSecurityAuditorAware.java
+++ src/main/java/com/specialized/SpringSecurityAuditorAware.java
@@ -2,12 +2,16 @@ package com.specialized;
 
 import org.springframework.data.domain.AuditorAware;
 
+/**
+ * This is used for user audit columns.
+ *
+ */
 public class SpringSecurityAuditorAware implements AuditorAware<Long> {
 
     @Override
     public Long getCurrentAuditor() {
         
-        // TODO: pull the id of the authenticated user.
+        // TODO: pull the id of the authenticated user.]
         
         return 1L;
     }
