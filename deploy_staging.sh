fbbfb49f814cc4327ad69f12940420607bea92fd
egstitt <evangstitt@gmail.com>
2016-11-15 11:32:02 -0800

    actually make deploy to staging work?

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
 echo "Deploying to staging"
-scp target/ride-recorder-api-0.1.0.jar ubuntu@35.163.102.162:
+ssh ubuntu@35.163.102.162 "sudo service ride-recorder-api stop"
+scp target/ride-recorder-api-0.1.0.jar ubuntu@35.163.102.162:/var/ride-recorder-api/
+ssh ubuntu@35.163.102.162 "sudo service ride-recorder-api start"
