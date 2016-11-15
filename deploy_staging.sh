651f8e106f23073c5ea5160a47772a34c3f496e3
egstitt <evangstitt@gmail.com>
2016-11-15 14:15:09 -0800

    simpler deploy script.

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
 echo "Deploying to staging"
-ssh ubuntu@35.163.102.162 "sudo service ride-recorder-api stop"
 scp target/ride-recorder-api-0.1.0.jar ubuntu@35.163.102.162:/var/ride-recorder-api/
-ssh ubuntu@35.163.102.162 "sudo service ride-recorder-api start"
+ssh ubuntu@35.163.102.162 "sudo service ride-recorder-api restart"
