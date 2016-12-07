443f4ad2007d92863c8aec7765d784a58277c41b
egstitt <evangstitt@gmail.com>
2016-12-06 22:45:27 -0800

    Empty commit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-echo "Deploying to AWS"
+echo "Deploying to EC2"
+
 scp target/rest-api-0.1.0.jar ubuntu@54.201.237.229:/var/rest-api/
 ssh ubuntu@54.201.237.229 "sudo service rest-api restart"
